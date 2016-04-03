1faa5af42a5c258119f72f450a97c2d7a7415d4a
David Hartunian <david@musicismath.net>
2016-04-03 16:58:14 -0400

    Remove useless Backbone Views
    - also cleaned up formatting in ReactHeader
      component

--- sources/js/jsx/header.js
+++ sources/js/jsx/header.js
@@ -1,16 +1,24 @@
 var ReactHeader = React.createClass({
 
     render: function() {
-        return (<header className="Header">
-            <a href={this.props.url} className="HeaderTitle">
-                {this.props.title}
-            </a>
+        return (
+            <header className="Header">
+                <a href={this.props.url} className="HeaderTitle">
+                    {this.props.title}
+                </a>
 
-            <ul className="HeaderItems">
-              <li className="HeaderItem"><a href='/' className="HeaderItem-link is-selected js-item js-map">Map</a></li>
-              <li className="HeaderItem"><a href='/about' className="HeaderItem-link js-item js-about">About</a></li>
-              <li className="HeaderItem"><a href='/privacy' className="HeaderItem-link js-item js-privacy">Privacy</a></li>
-            </ul>
-        </header>)
+                <ul className="HeaderItems">
+                  <li className="HeaderItem">
+                      <a href='/' className="HeaderItem-link is-selected js-item js-map">Map</a>
+                  </li>
+                  <li className="HeaderItem">
+                      <a href='/about' className="HeaderItem-link js-item js-about">About</a>
+                  </li>
+                  <li className="HeaderItem">
+                      <a href='/privacy' className="HeaderItem-link js-item js-privacy">Privacy</a>
+                  </li>
+                </ul>
+            </header>
+        )
     }
 });
